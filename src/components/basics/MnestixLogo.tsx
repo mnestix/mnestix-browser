5970411b108022633f24e1942da6dfd436ad89df
XITASO\pawel.baran <pawel.baran@xitaso.com>
2024-06-25 17:49:12 +0200

    MNES-1042: Updated default logo

---
+++ src/components/basics/MnestixLogo.tsx
@@ -0,0 +1,27 @@
+﻿import { styled, useTheme } from '@mui/material';
+import Logo from '../../assets/logo.svg';
+
+// May be extended with monochrome, reduced or other needed variants
+
+
+type MnestixLogoLogoProps = {
+    readonly width?: string;
+    readonly height?: string;
+};
+
+// Styling
+const StyledLogo = styled(Logo)<{ width?: string; height?: string }>(({ width, height }) => ({
+    maxWidth: '100%',
+    maxHeight: '100%',
+    width: width ?? '100%',
+    height: height ?? 'auto',
+    display: 'block',
+}));
+
+export function MnestixLogo(props: MnestixLogoLogoProps) {
+    // Differentiate between the logo variants
+    const theme = useTheme();
+    let color = theme.palette.common.white;
+    
+    return <StyledLogo width={props.width} height={props.height} color={color} />;
+}
