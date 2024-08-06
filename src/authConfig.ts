3f5a15012c92d2c0890a966f6eb1751b4b648201
Pawel Baran <98280057+pawel-baran-se@users.noreply.github.com>
2024-08-06 08:32:04 +0200

    Update src/authConfig.ts
    Co-authored-by: Milo Franke <milo.franke@xitaso.com>

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -32,7 +32,7 @@ export const authOptions: AuthOptions = {
         ] : [
             AzureADProvider({
                 clientId: process.env.AD_CLIENT_ID ? process.env.AD_CLIENT_ID : '',
-                clientSecret: process.env.AD_SECRET_VALUE ? process.env.AD_SECRET_VALUE : '',
+                clientSecret: process.env.AD_SECRET_VALUE ?? '',
                 tenantId: process.env.AD_TENANT_ID,
                 authorization: { params: { scope: `openid ${process.env.APPLICATION_ID_URI}admin.write` } },
             })
