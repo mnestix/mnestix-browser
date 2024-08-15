7e09ce22aba2cfeff78e2cd37d091421cc3773a9
Jonathan Sigrist <jonathan.sigrist@xitaso.com>
2024-08-15 15:50:01 +0200

    snackbars for errors

--- src/app/[locale]/_components/QrScanner.tsx
+++ src/app/[locale]/_components/QrScanner.tsx
@@ -2,18 +2,21 @@
 
 import { useState } from 'react';
 import ScannerLogo from 'assets/ScannerLogo.svg';
-import { Box, useTheme } from '@mui/material';
+import { Alert, Box, useTheme } from '@mui/material';
 import { CenteredLoadingSpinner } from 'components/basics/CenteredLoadingSpinner';
 import { QrStream } from 'app/[locale]/_components/QrStream';
+import { Snackbar } from '@mui/material';
 
-export function QrScanner(props: { onScan: (scanResult: string) => Promise<void> }) {
+export function QrScanner(props: { onScan: (scanResult: string) => Promise<void>; callbackErrorMsg?: string | undefined }) {
     const [isQrActive, setIsQrActive] = useState<boolean>(false);
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const theme = useTheme();
     const logoStyle = {
-        color: theme.palette.primary.main,
+        color: theme.palette.primary.main
     };
+
+    const callbackErrorMsg = props.callbackErrorMsg || 'Could not open the QR code!';
 
     // const stopQrScanner = () => {
     //     scanner?.current?.stop();
@@ -28,7 +31,10 @@ export function QrScanner(props: { onScan: (scanResult: string) => Promise<void>
 
     const switchToVideoStream = (loadingSuccessful: boolean) => {
         setIsLoading(false);
-        if (!loadingSuccessful) setIsQrActive(false);
+        if (!loadingSuccessful) {
+            setIsQrActive(false);
+            setIsCameraError(true);
+        }
     };
 
     const handleScan = async (result: string) => {
@@ -37,30 +43,62 @@ export function QrScanner(props: { onScan: (scanResult: string) => Promise<void>
             setIsQrActive(false);
             await props.onScan(result);
         } catch {
+            setIsCallbackError(true);
             setIsQrActive(true);
         }
         setIsLoading(false);
     };
 
+    // Snackbar content
+
+    const [isCallbackError, setIsCallbackError] = useState<boolean>(false);
+    const [isCameraError, setIsCameraError] = useState<boolean>(false);
+
+    const handleCallbackErrorSnackbarClose = () => {
+        setIsCallbackError(false);
+    };
+
+    const handleCameraErrorSnackbarClose = () => {
+        setIsCameraError(false);
+    };
+
+    //
+
     return (
-        <Box
-            style={{
-                cursor: 'pointer',
-                height: 250,
-                width: 250,
-                marginLeft: 'auto',
-                marginRight: 'auto',
-            }}
-        >
-            {!isQrActive && !isLoading && (
-                <Box onClick={startQrScanner}>
-                    <ScannerLogo style={logoStyle} alt="Scanner Logo" />
-                </Box>
-            )}
-            {isLoading && <CenteredLoadingSpinner />}
-            {isQrActive && (
-                <QrStream onScan={handleScan} onLoadingFinished={switchToVideoStream} />
-            )}
-        </Box>
+        <>
+            <Box
+                style={{
+                    cursor: 'pointer',
+                    height: 250,
+                    width: 250,
+                    marginLeft: 'auto',
+                    marginRight: 'auto'
+                }}
+            >
+                {!isQrActive && !isLoading && (
+                    <Box onClick={startQrScanner}>
+                        <ScannerLogo style={logoStyle} alt="Scanner Logo" />
+                    </Box>
+                )}
+                {isLoading && <CenteredLoadingSpinner />}
+                {isQrActive && <QrStream onScan={handleScan} onLoadingFinished={switchToVideoStream} />}
+            </Box>
+            <Snackbar open={isCallbackError} autoHideDuration={4000} onClose={handleCallbackErrorSnackbarClose}>
+                <Alert severity="error" variant="filled" sx={{ width: '100%' }}
+                       onClose={handleCallbackErrorSnackbarClose}>
+                    {callbackErrorMsg}
+                </Alert>
+            </Snackbar>
+            <Snackbar open={isCameraError} autoHideDuration={4000} onClose={handleCameraErrorSnackbarClose}>
+                <Alert
+                    severity="error"
+                    variant="filled"
+                    sx={{ width: '100%' }}
+                    onClose={handleCameraErrorSnackbarClose}
+                >
+                    Could not open the QR scanner!
+                </Alert>
+            </Snackbar>
+        </>
     );
 }
