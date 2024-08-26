e40879eb58bf037880d441362d48f3a9669c298b
melanie.gail <melanie.gail@xitaso.com>
2024-08-26 10:31:05 +0200

    MNES-1186 return enum value and extend start command

--- src/app/api/mnestixConnections/route.ts
+++ src/app/api/mnestixConnections/route.ts
@@ -2,9 +2,13 @@ import { prisma } from 'lib/database/prisma';
 import { NextRequest } from 'next/server';
 
 export async function GET() {
-    const mnestixConnections = await prisma.mnestixConnection.findMany()
-    
-    return Response.json(mnestixConnections);
+    try {
+        const mnestixConnections = await prisma.mnestixConnection.findMany({include: {type: true}})
+
+        return Response.json(mnestixConnections);
+    } catch (error) {
+        return Response.json({ error: (error as Error).message });
+    }
 }
 
 export async function POST(req: NextRequest) {
