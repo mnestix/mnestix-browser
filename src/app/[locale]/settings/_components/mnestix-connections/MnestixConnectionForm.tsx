8875ef6d32b6b8e7dab4ca9d9ecacb3271631833
XITASO\pawel.baran <pawel.baran@xitaso.com>
2024-09-05 12:35:51 +0200

    MNES-1209: Submodel Repositories added - init

---
+++ src/app/[locale]/settings/_components/mnestix-connections/MnestixConnectionForm.tsx
@@ -0,0 +1,115 @@
+import { Box, Button, Divider, FormControl, IconButton, Skeleton, TextField, Typography } from '@mui/material';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { messages } from 'lib/i18n/localization';
+import { Dispatch, Fragment, SetStateAction } from 'react';
+import ControlPointIcon from '@mui/icons-material/ControlPoint';
+import { Control, Controller, FieldArrayWithId, useFieldArray, UseFormGetValues } from 'react-hook-form';
+import { ConnectionFormData } from 'app/[locale]/settings/_components/mnestix-connections/MnestixConnectionsCard';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+
+export type MnestixConnectionsFormProps = {
+    readonly connectionType: string;
+    readonly defaultUrl: string | undefined;
+    readonly isLoading: boolean;
+    readonly isEditMode: boolean;
+    readonly setIsEditMode: Dispatch<SetStateAction<boolean>>;
+    readonly control: Control<ConnectionFormData, never>;
+    readonly getValues: UseFormGetValues<ConnectionFormData>;
+}
+
+export function MnestixConnectionsForm(props: MnestixConnectionsFormProps) {
+    const { connectionType, defaultUrl, getValues, isLoading, setIsEditMode, isEditMode } = props;
+    const control = props.control as Control<ConnectionFormData, never>;
+    const intl = useIntl();
+    
+    const { fields, append, remove } = useFieldArray<ConnectionFormData>({
+        control,
+        name: 'repositories',
+    });
+    
+    const dataConnectionType = intl.formatMessage(messages.mnestix.connections[connectionType].connectionType);
+
+    function getFormControl(field: FieldArrayWithId<ConnectionFormData, 'repositories', 'id'>, index: number) {
+        return (
+            field.type === dataConnectionType && (
+                <FormControl fullWidth variant="filled" key={field.id}>
+                    <Box display="flex" flexDirection="row" mb={2} alignItems="center">
+                        <Typography variant="h4" mr={4} width="200px">
+                            <FormattedMessage {...messages.mnestix.connections[connectionType].repositoryLabel} />
+                        </Typography>
+                        {isEditMode ? (
+                            <Box display="flex" alignItems="center" width="100%">
+                                <Controller
+                                    name={`repositories.${index}.url`}
+                                    control={control}
+                                    defaultValue={field.url}
+                                    rules={{
+                                        required: intl.formatMessage(messages.mnestix.connections.urlFieldRequired),
+                                    }}
+                                    render={({ field, fieldState: { error } }) => (
+                                        <TextField
+                                            {...field}
+                                            label={
+                                                <FormattedMessage
+                                                    {...messages.mnestix.connections[connectionType].repositoryUrlLabel}
+                                                />
+                                            }
+                                            sx={{ flexGrow: 1, mr: 1 }}
+                                            fullWidth={true}
+                                            error={!!error}
+                                            helperText={error ? error.message : ''}
+                                        />
+                                    )}
+                                />
+                                <IconButton>
+                                    <RemoveCircleOutlineIcon onClick={() => remove(index)} />
+                                </IconButton>
+                            </Box>
+                        ) : (
+                            <Typography mb={2} mt={2}>
+                                {getValues(`repositories.${index}.url`)}
+                            </Typography>
+                        )}
+                    </Box>
+                </FormControl>
+            )
+        );
+    }
+    
+    return (
+        <Box sx={{ my: 2 }}>
+            <Divider/>
+            <Typography variant="h3" sx={{ my: 2 }}>
+                <FormattedMessage {...messages.mnestix.connections[connectionType].repositories} />
+            </Typography>
+            <Box display="flex" flexDirection="row" mb={4} alignItems="center">
+                <Typography variant="h4" mr={4} width="200px">
+                    <FormattedMessage {...messages.mnestix.connections[connectionType].repositoryDefaultLabel} />
+                </Typography>
+                <Typography>{defaultUrl}</Typography>
+            </Box>
+            {isLoading &&
+                !fields.length &&
+                [0, 1, 2].map((i) => {
+                    return (
+                        <Fragment key={i}>
+                            <Skeleton variant="text" width="50%" height={26} sx={{ m: 2 }}/>
+                        </Fragment>
+                    );
+                })}
+            {!isLoading && fields.map((field, index) => getFormControl(field, index))}
+            <Box>
+                <Button
+                    variant="text"
+                    startIcon={<ControlPointIcon/>}
+                    onClick={() => {
+                        setIsEditMode(true);
+                        append({ id: 'temp', type: dataConnectionType , url: '' });
+                    }}
+                >
+                    <FormattedMessage {...messages.mnestix.connections.addButton} />
+                </Button>
+            </Box>
+        </Box>
+    )
+}
--- src/app/[locale]/settings/_components/mnestix-connections/MnestixConnectionsCard.tsx
+++ src/app/[locale]/settings/_components/mnestix-connections/MnestixConnectionsCard.tsx
@@ -1,17 +1,16 @@
 import { FormattedMessage, useIntl } from 'react-intl';
 import { messages } from 'lib/i18n/localization';
-import { Box, Button, Divider, FormControl, IconButton, Skeleton, TextField, Typography } from '@mui/material';
-import { Fragment, useState } from 'react';
+import { Box } from '@mui/material';
+import { useState } from 'react';
 import { useNotificationSpawner } from 'lib/hooks/UseNotificationSpawner';
 import {
     getConnectionDataAction,
     upsertConnectionDataAction,
 } from 'app/[locale]/settings/_components/mnestix-connections/MnestixConnectionServerActions';
-import ControlPointIcon from '@mui/icons-material/ControlPoint';
-import { Controller, FieldArrayWithId, useFieldArray, useForm } from 'react-hook-form';
-import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import {  useForm } from 'react-hook-form';
 import { useEnv } from 'app/env/provider';
 import { SettingsCardHeader } from 'app/[locale]/settings/_components/SettingsCardHeader';
+import { MnestixConnectionsForm } from 'app/[locale]/settings/_components/mnestix-connections/MnestixConnectionForm';
 
 export type ConnectionFormData = {
     repositories: {
@@ -63,11 +62,6 @@ export function MnestixConnectionsCard() {
         reset,
     } = useForm<ConnectionFormData>({ defaultValues: async () => await mapFormData() });
 
-    const { fields, append, remove } = useFieldArray<ConnectionFormData>({
-        control,
-        name: 'repositories',
-    });
-
     async function saveConnectionData(data: ConnectionFormData) {
         try {
             await upsertConnectionDataAction(data);
@@ -85,45 +79,7 @@ export function MnestixConnectionsCard() {
         reset();
         setIsEditMode(false);
     };
-
-    function getFormControl(field: FieldArrayWithId<ConnectionFormData, 'repositories', 'id'>, index: number) {
-        return (
-            <FormControl fullWidth variant="filled" key={field.id}>
-                <Box display="flex" flexDirection="row" mb={2} alignItems="center">
-                    <Typography variant="h4" mr={4} width="160px">
-                        <FormattedMessage {...messages.mnestix.connections.repositoryLabel} />
-                    </Typography>
-                    {isEditMode ? (
-                        <Box display="flex" alignItems="center" width="100%">
-                            <Controller
-                                name={`repositories.${index}.url`}
-                                control={control}
-                                defaultValue={field.url}
-                                rules={{ required: intl.formatMessage(messages.mnestix.connections.urlFieldRequired) }}
-                                render={({ field, fieldState: { error } }) => (
-                                    <TextField
-                                        {...field}
-                                        label={
-                                            <FormattedMessage {...messages.mnestix.connections.repositoryUrlLabel} />}
-                                        sx={{ flexGrow: 1, mr: 1 }}
-                                        fullWidth={true}
-                                        error={!!error}
-                                        helperText={error ? error.message : ''}
-                                    />
-                                )}
-                            />
-                            <IconButton>
-                                <RemoveCircleOutlineIcon onClick={() => remove(index)}/>
-                            </IconButton>
-                        </Box>
-                    ) : (
-                        <Typography mb={2} mt={2}>{getValues(`repositories.${index}.url`)}</Typography>
-                    )}
-                </Box>
-            </FormControl>
-        );
-    }
-
+    
     return (
         <Box sx={{ p: 3, width: '100%' }}>
             <SettingsCardHeader title={<FormattedMessage {...messages.mnestix.connections.title} />}
@@ -131,40 +87,20 @@ export function MnestixConnectionsCard() {
                                 onCancel={() => cancelEdit()} onEdit={() => setIsEditMode(true)}
                                 onSubmit={handleSubmit((data) => saveConnectionData(data))}
                                 isEditMode={isEditMode}/>
-            <Box sx={{ my: 2 }}>
-                <Divider/>
-                <Typography variant="h3" color="primary" sx={{ my: 2 }}>
-                    <FormattedMessage {...messages.mnestix.connections.repositories} />
-                </Typography>
-                <Box display="flex" flexDirection="row" mb={4} alignItems="center">
-                    <Typography variant="h4" mr={4} width="200px">
-                        <FormattedMessage {...messages.mnestix.connections.repositoryDefaultLabel} />
-                    </Typography>
-                    <Typography>{env.AAS_REPO_API_URL}</Typography>
-                </Box>
-                {isLoading &&
-                    !fields.length &&
-                    [0, 1, 2].map((i) => {
-                        return (
-                            <Fragment key={i}>
-                                <Skeleton variant="text" width="50%" height={26} sx={{ m: 2 }}/>
-                            </Fragment>
-                        );
-                })}
-                {!isLoading && fields.map((field, index) => getFormControl(field, index))}
-                <Box>
-                    <Button
-                        variant="text"
-                        startIcon={<ControlPointIcon/>}
-                        onClick={() => {
-                            setIsEditMode(true);
-                            append({ id: 'temp', type: 'AAS_REPOSITORY', url: '' });
-                        }}
-                    >
-                        <FormattedMessage {...messages.mnestix.connections.addButton} />
-                    </Button>
-                </Box>
-            </Box>
+            <MnestixConnectionsForm connectionType={'aasRepository'}
+                                    defaultUrl={env.AAS_REPO_API_URL} 
+                                    isLoading={isLoading} 
+                                    isEditMode={isEditMode} 
+                                    setIsEditMode={setIsEditMode} 
+                                    control={control} 
+                                    getValues={getValues}/>
+            <MnestixConnectionsForm connectionType={'submodelRepository'}
+                                    defaultUrl={env.AAS_REPO_API_URL}
+                                    isLoading={isLoading}
+                                    isEditMode={isEditMode}
+                                    setIsEditMode={setIsEditMode}
+                                    control={control}
+                                    getValues={getValues}/>
         </Box>
     );
 }
