e9de0e8907c8b7f6a59693768788a6a7935f214c
milo.franke <milo.franke@xitaso.com>
2024-09-13 11:02:53 +0200

    fix navigation in discovery list when clicking on image

--- src/app/[locale]/viewer/discovery/_components/DiscoveryListTableRow.tsx
+++ src/app/[locale]/viewer/discovery/_components/DiscoveryListTableRow.tsx
@@ -31,7 +31,7 @@ export const DiscoveryListTableRow = (props: DiscoveryListTableRowProps) => {
 
     return (
         <>
-            <PictureTableCell onClickAction={() => navigateToAas(aasListEntry.aasId)} />
+            <PictureTableCell onClickAction={() => navigateToAas(aasListEntry.aasId, aasListEntry.repositoryUrl)} />
             <TableCell align="left" sx={tableBodyText}>
                 {aasListEntry.aasId}
             </TableCell>
