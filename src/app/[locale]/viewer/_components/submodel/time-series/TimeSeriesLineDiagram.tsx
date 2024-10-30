85348653e56acad526d9d3c83dff2be0d8f720cc
Andre Jentzsch <andre.jentzsch@xitaso.com>
2024-10-30 08:47:05 +0100

    MNES-1276: Changing color to forrest

--- src/app/[locale]/viewer/_components/submodel/time-series/TimeSeriesLineDiagram.tsx
+++ src/app/[locale]/viewer/_components/submodel/time-series/TimeSeriesLineDiagram.tsx
@@ -10,7 +10,7 @@ import {
     XAxis,
     YAxis,
 } from 'recharts';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
 import { TimeSeriesDataSet } from 'app/[locale]/viewer/_components/submodel/time-series/TimeSeriesUtil';
 
 function formatDate(dateString: string, onlyTime = false) {
@@ -18,11 +18,11 @@ function formatDate(dateString: string, onlyTime = false) {
 
     const options: Intl.DateTimeFormatOptions = {
         year: onlyTime ? undefined : '2-digit',
-        month: onlyTime ? undefined :  '2-digit',
+        month: onlyTime ? undefined : '2-digit',
         day: onlyTime ? undefined : '2-digit',
         hour: '2-digit',
         minute: onlyTime ? '2-digit' : undefined,
-        second: onlyTime ? '2-digit' : undefined
+        second: onlyTime ? '2-digit' : undefined,
     };
     return date.toLocaleString('en-US', options);
 }
@@ -52,6 +52,8 @@ function getUTCMidnightEquivalentTime(dates: string[]) {
 export function TimeSeriesLineDiagram(props: { data: TimeSeriesDataSet; timeframeSelectable?: boolean }) {
     const uniqueDates = [...new Set(props.data.points.map((point) => getDateStamp(point['timestamp'] as string)))];
     const startDayMarkerStamp = uniqueDates.length > 2 ? getUTCMidnightEquivalentTime(uniqueDates) : [];
+    const theme = useTheme();
+    const color = theme.palette.primary.main;
 
     const CustomTooltip = ({
         active,
@@ -74,7 +76,10 @@ export function TimeSeriesLineDiagram(props: { data: TimeSeriesDataSet; timefram
                 >
                     <Typography>{formatDate(label, uniqueDates.length <= 2)}</Typography>
                     {payload.map((p, index) => (
-                        <Box key={index} sx={{ fontSize: 11, color: p.color, paddingY: '4px' }}>{`${p.name} : ${p.value} `}</Box>
+                        <Box
+                            key={index}
+                            sx={{ fontSize: 11, color: p.color, paddingY: '4px' }}
+                        >{`${p.name} : ${p.value} `}</Box>
                     ))}
                 </Box>
             );
@@ -87,8 +92,12 @@ export function TimeSeriesLineDiagram(props: { data: TimeSeriesDataSet; timefram
             <ResponsiveContainer>
                 <LineChart data={props.data.points} margin={{ top: 5, right: 30, left: -20, bottom: 5 }}>
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis fontSize={11} dataKey="timestamp" tickFormatter={(v) => formatDate(v, uniqueDates.length <= 2)} />
-                    <YAxis fontSize={11}/>
+                    <XAxis
+                        fontSize={11}
+                        dataKey="timestamp"
+                        tickFormatter={(v) => formatDate(v, uniqueDates.length <= 2)}
+                    />
+                    <YAxis fontSize={11} />
                     <Tooltip content={<CustomTooltip />} />
                     {startDayMarkerStamp.map((marker) => (
                         <ReferenceLine key={marker} x={marker} stroke="blue" isFront>
@@ -102,14 +111,20 @@ export function TimeSeriesLineDiagram(props: { data: TimeSeriesDataSet; timefram
                         </ReferenceLine>
                     ))}
                     {props.data.names.map((name, index) => (
-                        <Line type="monotone" key={index} dataKey={name} stroke="#5e6b7c" />
+                        <Line
+                            type="monotone"
+                            key={index}
+                            dataKey={name}
+                            stroke={color}
+                            dot={{ stroke: color, fill: color, strokeWidth: 1, r: 3 }}
+                        />
                     ))}
                     {props.timeframeSelectable && (
                         <Brush
                             dataKey="timestamp"
                             height={30}
                             tickFormatter={(v) => formatDate(v, uniqueDates.length <= 2)}
-                            stroke="#5e6b7c"
+                            stroke={color}
                         />
                     )}
                 </LineChart>
