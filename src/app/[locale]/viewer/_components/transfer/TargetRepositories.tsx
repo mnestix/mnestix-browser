a5c3302a7c04f0a288a18b7fc779425f89b67bb9
melanie.gail <melanie.gail@xitaso.com>
2024-11-07 10:12:26 +0100

    Store ApiKey in LocalStorage

--- src/app/[locale]/viewer/_components/transfer/TargetRepositories.tsx
+++ src/app/[locale]/viewer/_components/transfer/TargetRepositories.tsx
@@ -14,7 +14,7 @@ import {
     getConnectionDataByTypeAction
 } from 'lib/services/database/connectionServerActions';
 import { useNotificationSpawner } from 'lib/hooks/UseNotificationSpawner';
-import { Fragment, useState } from 'react';
+import { ChangeEvent, Fragment, useState } from 'react';
 import { useAsyncEffect } from 'lib/hooks/UseAsyncEffect';
 import { ConnectionTypeEnum } from 'lib/services/database/ConnectionTypeEnum';
 import { Controller, useForm } from 'react-hook-form';
@@ -45,13 +45,20 @@ export function TargetRepositories(props: TargetRepositoryProps) {
     useAsyncEffect(async () => {
         try {
             setIsLoading(true);
-            const aasRepositories = await getConnectionDataByTypeAction({ id: '0', typeName: ConnectionTypeEnum.AAS_REPOSITORY });
-            if(env.AAS_REPO_API_URL) aasRepositories.push(env.AAS_REPO_API_URL)
+            const aasRepositories = await getConnectionDataByTypeAction({
+                id: '0',
+                typeName: ConnectionTypeEnum.AAS_REPOSITORY
+            });
+            if (env.AAS_REPO_API_URL) aasRepositories.push(env.AAS_REPO_API_URL)
             setAasRepositories(aasRepositories);
-            const submodelRepositories = await getConnectionDataByTypeAction({ id: '2', typeName: ConnectionTypeEnum.SUBMODEL_REPOSITORY });
-            if(env.SUBMODEL_REPO_API_URL) submodelRepositories.push(env.SUBMODEL_REPO_API_URL)
+            const submodelRepositories = await getConnectionDataByTypeAction({
+                id: '2',
+                typeName: ConnectionTypeEnum.SUBMODEL_REPOSITORY
+            });
+            if (env.SUBMODEL_REPO_API_URL) submodelRepositories.push(env.SUBMODEL_REPO_API_URL)
+            
             setSubmodelRepositories(submodelRepositories);
-        } catch(error) {
+        } catch (error) {
             notificationSpawner.spawn({
                 message: error,
                 severity: 'error',
@@ -61,16 +68,26 @@ export function TargetRepositories(props: TargetRepositoryProps) {
         }
         return;
     }, []);
-
-    const { handleSubmit, control, formState } = useForm();
     
+    const defaultValues = {
+        repository: '',
+        submodelRepository: '0',
+        repositoryApiKey: localStorage.getItem('TransferApiKey') || ''
+    }
+
+    const { handleSubmit, control, formState } = useForm({ defaultValues });
+
     const onSubmit = (data: TargetRepositoryFormData) => {
         props.onSubmitStep(data);
     }
 
     const handleClickShowApikey = () => setShowApikey((show) => !show);
 
-    return(
+    const saveApiKeyToLocalStorage = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        localStorage.setItem('TransferApiKey', event.target.value);
+    }
+
+    return (
         <>
             {isLoading ?
                 [0, 1].map((i) => {
@@ -84,66 +101,75 @@ export function TargetRepositories(props: TargetRepositoryProps) {
                     <Box display="flex" flexDirection="column">
                         <Box display="flex" flexDirection="row" alignItems="flex-start">
                             <Typography variant="h4"
-                                        sx={{ minWidth: '250px', mr: 2 }}><FormattedMessage {...messages.mnestix.transfer.chooseRepository} /></Typography>
+                                        sx={{
+                                            minWidth: '250px',
+                                            mr: 2
+                                        }}><FormattedMessage {...messages.mnestix.transfer.chooseRepository} /></Typography>
                             <Box display="flex" flexDirection="column" width="100%">
-                            <FormControl fullWidth>
-                                <Controller
-                                    name="repository"
-                                    control={control}
-                                    defaultValue=""
-                                    rules={{ required: intl.formatMessage(messages.mnestix.transfer.repositoryRequired) }}
-                                    render={({ field, fieldState }) => (
-                                        <TextField fullWidth 
-                                                   select
-                                                   label={intl.formatMessage(messages.mnestix.transfer.repositoryLabel)}
-                                                   error={!!fieldState.error}
-                                                   helperText={fieldState.error ? fieldState.error.message : null}
-                                                   required {...field}>
-                                            { aasRepositories.map((repo, index) => {
-                                            return <MenuItem key={index} value={repo}>{repo}</MenuItem>
-                                        })}
-                                        </TextField>)}
-                                />
-                            </FormControl>
-                            <FormControl fullWidth sx={{ mt: 2 }}>
-                                <Controller
-                                    name="repositoryApiKey"
-                                    control={control}
-                                    defaultValue=""
-                                    render={({ field }) => (
-                                        <TextField type={showApikey ? 'text' : 'password'}
-                                                   InputProps={{
+                                <FormControl fullWidth>
+                                    <Controller
+                                        name="repository"
+                                        control={control}
+                                        rules={{ required: intl.formatMessage(messages.mnestix.transfer.repositoryRequired) }}
+                                        render={({ field, fieldState }) => (
+                                            <TextField fullWidth
+                                                       select
+                                                       label={intl.formatMessage(messages.mnestix.transfer.repositoryLabel)}
+                                                       error={!!fieldState.error}
+                                                       helperText={fieldState.error ? fieldState.error.message : null}
+                                                       required {...field}>
+                                                {aasRepositories.map((repo, index) => {
+                                                    return <MenuItem key={index} value={repo}>{repo}</MenuItem>
+                                                })}
+                                            </TextField>)}
+                                    />
+                                </FormControl>
+                                <FormControl fullWidth sx={{ mt: 2 }}>
+                                    <Controller
+                                        name="repositoryApiKey"
+                                        control={control}
+                                        render={({ field }) => (
+                                            <TextField type={showApikey ? 'text' : 'password'}
+                                                       InputProps={{
                                                            endAdornment:
-                                                           <InputAdornment position="end">
-                                                               <IconButton
-                                                               onClick={handleClickShowApikey}
-                                                               edge="end"
-                                                                >
-                                                                    {showApikey ? <VisibilityOff/> : <Visibility/>}
-                                                                </IconButton>
-                                                            </InputAdornment>
-                                                    }}
-                                                   label={intl.formatMessage(messages.mnestix.transfer.repositoryApiKey)}
-                                                   {...field}/>)}
-                                />
-                            </FormControl>
+                                                               <InputAdornment position="end">
+                                                                   <IconButton
+                                                                       onClick={handleClickShowApikey}
+                                                                       edge="end"
+                                                                   >
+                                                                       {showApikey ? <VisibilityOff/> : <Visibility/>}
+                                                                   </IconButton>
+                                                               </InputAdornment>
+                                                       }}
+                                                       {...field}
+                                                       onChange={(event) => {
+                                                           field.onChange(event);
+                                                           saveApiKeyToLocalStorage(event)
+                                                       }}
+                                                       label={intl.formatMessage(messages.mnestix.transfer.repositoryApiKey)}
+                                            />)}
+                                    />
+                                </FormControl>
                             </Box>
                         </Box>
                         <Box display="flex" flexDirection="row" mt={5} alignItems="flex-start">
                             <Typography variant="h4"
-                                        sx={{ minWidth: '250px', mr: 2 }}><FormattedMessage {...messages.mnestix.transfer.chooseSubmodelRepository} /></Typography>
+                                        sx={{
+                                            minWidth: '250px',
+                                            mr: 2
+                                        }}><FormattedMessage {...messages.mnestix.transfer.chooseSubmodelRepository} /></Typography>
                             <FormControl fullWidth>
                                 <Controller
                                     name="submodelRepository"
                                     control={control}
-                                    defaultValue="0"
                                     render={({ field }) => (
                                         <TextField fullWidth
                                                    select
                                                    label={intl.formatMessage(messages.mnestix.transfer.submodelRepositoryLabel)}
                                                    {...field}>
-                                            <MenuItem key="none" value="0"><FormattedMessage {...messages.mnestix.transfer.useAasRepository} /></MenuItem>
-                                            { submodelRepositories.map((repo, index) => {
+                                            <MenuItem key="none"
+                                                      value="0"><FormattedMessage {...messages.mnestix.transfer.useAasRepository} /></MenuItem>
+                                            {submodelRepositories.map((repo, index) => {
                                                 return <MenuItem key={index} value={repo}>{repo}</MenuItem>
                                             })}
                                         </TextField>)}
@@ -152,8 +178,9 @@ export function TargetRepositories(props: TargetRepositoryProps) {
                         </Box>
                         <Divider sx={{ mt: 6, mb: 4 }}/>
                         <DialogActions>
-                            <LoadingButton sx={{ mr: 1 }} variant="outlined" disabled={!formState.isValid} loading={props.isSubmitting}
-                                    onClick={handleSubmit(onSubmit)}><FormattedMessage {...messages.mnestix.transfer.title} /></LoadingButton>
+                            <LoadingButton sx={{ mr: 1 }} variant="outlined" disabled={!formState.isValid}
+                                           loading={props.isSubmitting}
+                                           onClick={handleSubmit(onSubmit)}><FormattedMessage {...messages.mnestix.transfer.title} /></LoadingButton>
                         </DialogActions>
                     </Box>
                 </form>
