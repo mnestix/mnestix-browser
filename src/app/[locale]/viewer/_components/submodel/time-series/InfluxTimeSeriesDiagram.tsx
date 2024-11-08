565e5393f366728310081a5a24bfe0026dfa8cf3
XITASO\pawel.baran <pawel.baran@xitaso.com>
2024-11-08 08:53:31 +0100

    MNES-1320: handle Mnestix Api Proxy for Influx DB

--- src/app/[locale]/viewer/_components/submodel/time-series/InfluxTimeSeriesDiagram.tsx
+++ src/app/[locale]/viewer/_components/submodel/time-series/InfluxTimeSeriesDiagram.tsx
@@ -16,12 +16,18 @@ export function InfluxTimeSeriesDiagram(props: { query: string; endpoint: string
     useEffect(() => {
         const responseData: TimeSeriesDataSet = { points: [], names: ['y'] };
         const parsedUrl = new URL(endpoint);
-        const org = parsedUrl.searchParams.get('org') ?? ''
-        const token = parsedUrl.searchParams.get('token') ?? ''
-        const url = parsedUrl.origin
+        const org = parsedUrl.searchParams.get('org') ?? '';
+        const token = parsedUrl.searchParams.get('token') ?? '';
+        const pathName = parsedUrl.pathname;
+        // To maintain backward compatibility in configuration,
+        // we add the /influx endpoint to route data requests through the Mnestix Proxy.
+        // This approach securely manages the access token using environment variables,
+        // ensuring that sensitive information remains hidden.
+        const url = parsedUrl.origin + (pathName.includes('/influx/') ? '/influx' : '');
 
         const influx = new InfluxDB({
-            url, headers: { 'Authorization': ('Token ' + token) }
+            url,
+            headers: { Authorization: 'Token ' + token },
         }).getQueryApi(org);
         const observer: FluxResultObserver<string[]> = {
             next(row, tableMeta) {
